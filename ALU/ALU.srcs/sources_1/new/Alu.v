2caea080a2c0163c26e60fffd5bdabd257af39a6
Shishir Yalburgi <shishiryalburgi@gmail.com>
2023-09-01 14:00:25 -0500

    Final

--- ALU/ALU.srcs/sources_1/new/Alu.v
+++ ALU/ALU.srcs/sources_1/new/Alu.v
@@ -24,28 +24,32 @@ module Alu(
     input [15:0] A,
     input [15:0] B,
     input [3:0] fn_sel,
+    input clk,
+    input rst,
     output reg [15:0] d_out,
-    output reg Z,
-    output reg C,
-    output reg N,
-    output reg V
+    output reg Zero_flag,
+    output reg Carry_flag
     );
     initial begin
-        Z = 1'b0;
-        V = 1'b0;
-        N = 1'b0;
-        C = 1'b0;
+        Zero_flag = 1'b0;
+        Carry_flag = 1'b0;
 	end
-always @ (A, B, fn_sel)
+always @ (posedge clk)
+begin
+if(rst)
+begin
+d_out = 16'b0000000000000000;
+Zero_flag = 1'b0;
+Carry_flag = 1'b0;
+end
+else
 begin
     case(fn_sel)
         4'b0000: begin
-            {C, d_out} = A+B;
+            {Carry_flag, d_out} = A+B;
         end
         4'b0001: begin 
             d_out = A-B;
-            if(d_out[15]==1) 
-                N = 1'b1;
         end 
         4'b0010: begin
             d_out = A * B; 
@@ -65,14 +69,15 @@ begin
         4'b0111: begin
             d_out = ~A; 
         end 
+        default:
+        d_out = 0;
+        
     endcase
     if(d_out == 0)
-        Z = 1'b1;
+        Zero_flag = 1'b1;
     else
-        Z= 0;
-	N = 1'b0;
-	C = 1'b0;
-	if((A[15]==1 & B[15]==1 & d_out[15]==1) | (A[15]==0 & B[15]==0 & d_out[15]==1))
-		V = 1'b1;
+        Zero_flag= 0;
+	Carry_flag = 1'b0;
     end 
+    end
 endmodule
